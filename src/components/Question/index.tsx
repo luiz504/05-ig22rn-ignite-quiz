84c377a76918f57b9faa7e11fd6513b6df5832b5
luiz bueno <luiz504@hotmail.com>
2024-03-10 15:08:27 -0300

    feat: keyframe

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,7 +1,11 @@
-import { View, Text } from 'react-native'
+import {
+  Text,
+  // Dimensions
+} from 'react-native'
 
 import { Option } from '../Option'
 import { styles } from './styles'
+import Animated from 'react-native-reanimated'
 
 type QuestionProps = {
   title: string
@@ -13,16 +17,45 @@ type Props = {
   alternativeSelected?: number | null
   setAlternativeSelected?: (value: number) => void
 }
+// const SCREEN_WIDTH = Dimensions.get('window').width
 
 export function Question({
   question,
   alternativeSelected,
   setAlternativeSelected,
 }: Props) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>{question.title}</Text>
+  // const enteringKeyFrame = new Keyframe({
+  //   0: {
+  //     opacity: 0,
+  //     transform: [{ translateX: SCREEN_WIDTH }, { rotate: '90deg' }],
+  //   },
+  //   70: {
+  //     opacity: 0.3,
+  //   },
+  //   100: {
+  //     opacity: 1,
+  //     transform: [{ translateX: 0 }, { rotate: '0deg' }],
+  //   },
+  // })
 
+  // const exitingKeyFrame = new Keyframe({
+  //   from: {
+  //     opacity: 1,
+  //     transform: [{ translateX: 0 }, { rotate: '0deg' }],
+  //   },
+  //   to: {
+  //     opacity: 0,
+  //     transform: [{ translateX: SCREEN_WIDTH * -1 }, { rotate: '-90deg' }],
+  //   },
+  // })
+
+  return (
+    <Animated.View
+      style={styles.container}
+      // entering={enteringKeyFrame}
+      // exiting={exitingKeyFrame}
+    >
+      <Text style={styles.title}>{question.title}</Text>
       {question.alternatives.map((alternative, index) => (
         <Option
           key={index}
@@ -33,6 +66,6 @@ export function Question({
           }
         />
       ))}
-    </View>
+    </Animated.View>
   )
 }
