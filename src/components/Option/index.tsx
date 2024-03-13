f6c999247a9b40b74f5c1996b40bdb291be8e0a8
luiz bueno <luiz504@hotmail.com>
2024-03-13 20:41:21 -0300

    feat: finishing option animations

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -7,6 +7,8 @@ import {
   useValue,
   runTiming,
   BlurMask,
+  Circle,
+  Easing,
 } from '@shopify/react-native-skia'
 
 import { styles } from './styles'
@@ -20,19 +22,23 @@ type Props = TouchableOpacityProps & {
 const CHECK_SIZE = 28
 const CHECK_STROKE = 2
 const CHECK_RADIUS = (CHECK_SIZE - CHECK_STROKE) / 2
+const INNER_CIRCLE_RADIUS = CHECK_RADIUS / 2
 
 export function Option({ checked, title, ...rest }: Props) {
   const path = Skia.Path.Make()
   path.addCircle(CHECK_SIZE, CHECK_SIZE, CHECK_RADIUS)
   const percentage = useValue(0)
+  const innerCircleSize = useValue(0)
 
   useEffect(() => {
     if (checked) {
       runTiming(percentage, 1, { duration: 500 })
+      runTiming(innerCircleSize, INNER_CIRCLE_RADIUS, { easing: Easing.bounce })
     } else {
       runTiming(percentage, 0, { duration: 500 })
+      runTiming(innerCircleSize, 0, { duration: 300 })
     }
-  }, [checked, percentage])
+  }, [checked, percentage, innerCircleSize])
   return (
     <TouchableOpacity
       style={[styles.container, checked && styles.checked]}
@@ -58,6 +64,14 @@ export function Option({ checked, title, ...rest }: Props) {
         >
           <BlurMask blur={1} style={'solid'} />
         </Path>
+        <Circle
+          cx={CHECK_SIZE}
+          cy={CHECK_SIZE}
+          r={innerCircleSize}
+          color={THEME.COLORS.BRAND_LIGHT}
+        >
+          <BlurMask blur={4} style={'solid'} />
+        </Circle>
       </Canvas>
     </TouchableOpacity>
   )
